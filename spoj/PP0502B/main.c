75823688fb65de006c07d8849801d68ca8479b5c
20563 <piotr@skajewski.com>
2017-04-11 02:15:35 +0200

    PP0502B - Tablice

---
+++ spoj/PP0502B/main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <inttypes.h>
+
+/*
+ * PP0502B - Tablice
+ * http://pl.spoj.com/problems/PP0502B/
+ * http://ideone.com/RQue89
+ */
+
+int main(void)
+{
+	int8_t t;
+	int8_t n;
+	int8_t *tablica;
+	int8_t element;
+	int8_t i = 0;
+
+	scanf("%" SCNd8, &t);
+	while (t--) {
+		scanf("%" SCNd8, &n);
+		tablica = (int8_t *)malloc(n * sizeof(int8_t));
+		i = 0;
+		while (n--) {
+			scanf("%" SCNd8, &element);
+			tablica[i] = element;
+			i++;
+		}
+		while (i--) {
+			printf("%" PRId8, tablica[i]);
+			if (i != 0)
+				printf(" ");
+		}
+		printf("\n");
+		free(tablica);
+	}
+	return 0;
+}
