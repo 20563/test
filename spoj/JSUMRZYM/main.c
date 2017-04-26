b39d0baf0635b84f451041a780581c0bd6f63e90
lukasz-kaczmarczyk <lukasz.kaczmarczyk@mail.com>
2017-04-26 14:51:05 +0200

    Update main.c
    added test functionality

--- spoj/JSUMRZYM/main.c
+++ spoj/JSUMRZYM/main.c
@@ -1,94 +1,25 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <inttypes.h>
-
-/** we can compile with flag -DNDEBUG or #define NDEBUG
- * but this only disable assert, not test function
- */
-
-#define UNIT_TEST
-#ifdef UNIT_TEST
 #include <assert.h>
-#include <string.h>
-#endif /** UNIT_TEST */
+#include <pthread.h>
+#include <time.h>
 
-/**
+/*
  * JSUMRZYM - Dodawanie rzymskie
  * http://pl.spoj.com/problems/JSUMRZYM/
  * http://ideone.com/edBLzT
  */
-
-static int16_t rta(const char *number);
-static const char *atr(int16_t number);
-
-#ifdef UNIT_TEST
-
-/**
- * check() - test to check function
- *
- * @a: first value
- * @b: second value
- * @input: pointer to expected value
- * @number: test number
- *
- * Return: display if test is done or fail,
- * and when fail then returns assert
- */
-
-static void check_atr(int8_t number, int16_t input,
-	const char *expected, int8_t action)
-{
-	char const *output = NULL;
-	int8_t c = 0;
-
-	output = atr(input);
-	c = strcmp(expected, output);
-
-	/** if expected fail then pass */
-
-	printf("test: %04" PRId8 " [%s] %s\n\n\t%04" PRId16 " %s %s %s\n\n",
-		number, c == 0 ? "pass" : "fail",
-		((c != 0 && action == 1) || c == 0)
-		? "[but expected]" : "[not expected]",
-		input, output, c == 0 ? "==" : "!=", expected);
-}
-
-/**
- * run_tests() - start all tests
- */
-
-static void run_tests(void)
-{
-	check_atr(1, 3, "III", 0);
-	check_atr(2, 1001, "MI", 0);
-	check_atr(3, 280, "CCLXXX", 0);
-	check_atr(4, 168, "CLXVIII", 0);
-	check_atr(5, 3888, "MMMDCCCLXXXVIII", 0);
-	check_atr(6, 3999, "MMMCMXCIX", 0);
-	check_atr(7, 18, "XVIII", 0);
-
-	/** good and marked as expected, allways good */
-	check_atr(8, 18, "XVIII", 1);
-	check_atr(9, 379, "CCCLXXIX", 0);
-
-	/** bad and marked as good there allways be error */
-	check_atr(10, 379, "CCCLXXI", 0);
-	check_atr(11, 9, "IX", 0);
-	check_atr(12, 24, "XXIV", 0);
-
-	/** bad but marked as expected */
-	check_atr(13, 123, "XXIVI", 1);
-
-	/** bad but marked as expected */
-	check_atr(14, 344, "I", 1);
-}
-
-#else
-static void run_tests(void) {}
-#endif /** UNIT_TEST */
+ 
+ #define FAULT -1
 
 const char roman[7] = {'I', 'V', 'X', 'L', 'C', 'D', 'M'};
 const int16_t arabic[7] = {1, 5, 10, 50, 100, 500, 1000};
+
+void check_tra(int16_t a, int16_t b, char *input, int8_t number);
+void time_trigger(void);
+void delay(double time_s);
+void check_rta(const char *input, const int16_t expected_result);
 
 /**
  * rta() - convert numbers roman to arabic
@@ -103,8 +34,14 @@ static int16_t rta(const char *number)
 {
 	int8_t i;
 	int16_t output = 0, tmp = 0;
+	
+	if (number == NULL)
+		return FAULT;
 
-	/** converting and checking if number offset in roman
+		
+	
+
+	/* converting and checking if number offset in roman
 	 * is lower than next offset, then substract that numer
 	 * from output value eg. IX, XC, CM.
 	 */
@@ -121,9 +58,19 @@ static int16_t rta(const char *number)
 		}
 		number++;
 	}
+	
+	output += tmp;
 
-	return output + tmp;
+	if (output > 3999)
+		return FAULT;
+		
+	return output;
 }
+
+/* opis funckcji zgdnie z kernelem  zmienne z podkreslnikiem
+ * zabazpieczyc funkcje przed przyjeciem zlych wartosci parametrow
+ * unit testy plan git commit push review
+ */
 
 /**
  * atr() - convert numbers arabic to roman
@@ -134,7 +81,7 @@ static int16_t rta(const char *number)
  * Return: return the number in roman notation
  */
 
-static const char *atr(int16_t number)
+static char *atr(int16_t number)
 {
 	int8_t chars_num = 0, mod = 0, i = 0;
 
@@ -142,19 +89,19 @@ static const char *atr(int16_t number)
 	 * 0 = 0, 1 = 1, 2 = 2, 3 = 3, 4 = 2, 5 = 1, 6 = 2, 7 = 3, 8 = 4, 9 = 2
 	 */
 
-	static int8_t map[] = {0, 1, 2, 3, 2, 1, 2, 3, 4, 2};
+	int8_t map[10] = {0, 1, 2, 3, 2, 1, 2, 3, 4, 2};
 	static char output[15] = {0};
 	char *out = output, tmp;
 
 	/* while the number is not zero convert arabic chars
-	 * to numbers repersentation and store result in reversed order
+	 * to numbers repersentation and store result in revesed order
 	 */
 
 	while (number) {
 		chars_num += map[number % 10];
 		mod = number % 5;
 		if (mod == 4) {
-			if ((number - 4) % 10 == 0)
+			if (((number - 4) % 10) == 0)
 				*out++ = roman[i+1];
 			else
 				*out++ = roman[i+2];
@@ -170,7 +117,7 @@ static const char *atr(int16_t number)
 		*out = '\0';
 	}
 
-	/**  reverse string to the proper order
+	/*  reverse string to the proper order
 	 */
 
 	for (i = 0; i < chars_num; i++) {
@@ -182,13 +129,22 @@ static const char *atr(int16_t number)
 	return output;
 }
 
-/**
+void run_test() {
+	check_rta(NULL, FAULT);
+	check_rta("MMMM", FAULT);
+	check_rta("MMMMCMXCIX", FAULT);
+	check_rta("III", 3);
+	check_rta("MCDXCIX", 1499);
+	check_rta("MMMCDLVII", 3457);
+}
+
+/*
  * longest number 3888 MMMDCCCLXXXVIII 15 chars
  * highest number 3999 MMMCMXCIX 12 bits
  */
 
 /**
- * main()
+ * main() - sum two numbers in arabic and return in roman
  *
  * @a: first number in roman
  * @b: second number in roman
@@ -200,23 +156,41 @@ static const char *atr(int16_t number)
 
 int main(void)
 {
-	char a[16] = {0}, b[16] = {0};
-	char const *output = NULL;
-/* watek w main 2 operacje atomowe mutex semafor */
-	/** we doesnt need to add #ifdef
-	 * because if UNIT_TEST is not defined
-	 * then function returns void
-	 */
-
-#ifdef UNIT_TEST
-	run_tests();
-#endif /** UNIT_TEST */
-
-	while (scanf("%15s %15s", a, b) == 2) {
-		output = atr(rta(a) + rta(b));
-		printf("%s\n", output);
+	pthread_t time_thread;
+	
+	if(pthread_create(&time_thread, NULL, time_trigger, NULL)) {
+		fprintf(stderr, "Error creating thread\n");
+		return 1;
 	}
-
+	
+	run_test();
+	delay(10);
 	return 0;
 }
 
+void check_rta(const char *input, const int16_t expected_result)
+{
+	int16_t result = 0;
+
+	result = rta(input);
+	(result == expected_result)? printf("test passed\n"):(printf("test failed\n"));
+}
+
+void time_trigger(void)
+{
+	while(1) {
+		delay(1);
+		printf("Hello World\n");
+
+	}
+	
+}
+
+void delay(double time_s) {
+    const clock_t start = clock();
+    clock_t current;
+    do{
+        current = clock();
+
+    }while((double)(current-start)/CLOCKS_PER_SEC < time_s);
+}
